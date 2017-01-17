ab8498dcd54f6eabc7f16f911be9dcc1fffda9fa
test <757384939@qq.com>
2017-01-17 16:17:52 +0800

    修复bug后工作完成提交

--- src/com/git/demo/File.java
+++ src/com/git/demo/File.java
@@ -9,6 +9,8 @@ public class File {
 		System.out.println("代码冲突：第二个开发人员修改");
 		System.out.println("代码冲突：第一个开发人员修改");
 		System.out.println("紧急修复bug，bug修复成功！");
+		System.out.println("stash：开始工作了。。。。。。。");
+		System.out.println("开始继续工作了.......开心:)");
 	}
 
 }
