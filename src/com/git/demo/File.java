9e2c41fe70da886cd8c482af4678ddb27900d035
test <757384939@qq.com>
2017-01-17 16:09:32 +0800

    紧急修复bug

--- src/com/git/demo/File.java
+++ src/com/git/demo/File.java
@@ -7,8 +7,8 @@ public class File {
 		System.out.println("Hello World!");
 		System.out.println("第一次修改");
 		System.out.println("代码冲突：第二个开发人员修改");
-
 		System.out.println("代码冲突：第一个开发人员修改");
+		System.out.println("紧急修复bug，bug修复成功！");
 	}
 
 }
